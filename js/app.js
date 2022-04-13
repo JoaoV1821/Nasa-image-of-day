5abd5e5b10cde59bd81162b3a2eb8be3e4e07c57
João Vitor <joaovitoraraujo1821@gmail.com>
2022-04-13 15:59:46 -0300

    Estilizei a página, fiz a lógica de validação de datas e defini uma data padrão

--- js/app.js
+++ js/app.js
@@ -1,28 +1,60 @@
-const getImage = function () {
-    const date = $('#date');
+const searchImage = function (date=getDataAtual()) {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=eqgmwhI5e8OsaSIzwvhtc82mZ6ehRcpi3wVLaBak&date=${date}`;
     const title = $('#title');
     const description = $('#description');
-    const img  = $('#nasa');
-    const url = `https://api.nasa.gov/planetary/apod?api_key=eqgmwhI5e8OsaSIzwvhtc82mZ6ehRcpi3wVLaBak&date=${date.val()}`;
+    const img = $('#image');
+    const video = $('#video');
 
     $.ajax({
         type: 'GET',
         url: url,
         success: function (response) {
-            img.attr('src', response.url);
             title.text(response.title);
             description.text(response.explanation);
+            console.log(response);
+
+            if (response.media_type == 'image') {
+                img.show();
+                video.hide();
+                img.attr('src', response.url);
+
+            } else if (response.media_type == 'video') {
+                img.hide();
+                video.show();
+                video.attr('src', response.url);
+            };
         }
     });
+};
+
+const getDataAtual = function () {
+    const date = new Date();
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+
+    return `${year}-${month+1}-${day}`;
 }
 
 const main = function () {
     const submit = $('#submit');
 
-    submit.click(function (event) { 
+    searchImage();
+
+    submit.click(function (event) {
         event.preventDefault();
-        getImage();
+        const date = $('#date').val();
+
+        const dataAtual = new Date(getDataAtual());
+        const dataFornecida = new Date(date);
+
+        if (dataFornecida.valueOf() > dataAtual.valueOf()) {
+            alert('Digite apenas datas até o dia atual');
+
+        } else {
+            searchImage(date);
+        };
     });
-}
+};
 
 main();
