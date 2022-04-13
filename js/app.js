7171f48cd71b2a09aeb075759943712816671292
João Vitor <joaovitoraraujo1821@gmail.com>
2022-04-13 14:02:39 -0300

    Iniciei a estilização da página

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
-const main = function() {
-    const img = $('#nasa');
-
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=eqgmwhI5e8OsaSIzwvhtc82mZ6ehRcpi3wVLaBak&date=2017-07-11'
+const getImage = function () {
+    const date = $('#date');
+    const title = $('#title');
+    const description = $('#description');
+    const img  = $('#nasa');
+    const url = `https://api.nasa.gov/planetary/apod?api_key=eqgmwhI5e8OsaSIzwvhtc82mZ6ehRcpi3wVLaBak&date=${date.val()}`;
 
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: url,
         success: function (response) {
-            console.log(response)
-            img.attr('src', response.url)
+            img.attr('src', response.url);
+            title.text(response.title);
+            description.text(response.explanation);
         }
     });
 }
 
-main()
+const main = function () {
+    const submit = $('#submit');
+
+    submit.click(function (event) { 
+        event.preventDefault();
+        getImage();
+    });
+}
+
+main();
